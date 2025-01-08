02940c6ce55e98afeba112c13039b233b2786a70
Dark01213 <154607506+Dark01213@users.noreply.github.com>
2025-01-08 23:26:18 +0100

    Add files via upload

---
+++ app/views/tasks/error.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Erreur 404</title>
+</head>
+<body>
+    <h1>Erreur 404</h1>
+    <p>La page demandée est introuvable.</p>
+    <a href="?page=home">Retour à l'accueil</a>
+</body>
+</html>
